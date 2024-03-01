7abad82c81aa36616fd3b61724593f98a39cf42b
Mark Slorach <hello@markslorach.com>
2024-03-01 21:23:06 +0000

    finished homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,36 @@
-import { ThemeToggle } from "./components/ThemeToggle";
+import { Button } from "@/components/ui/button";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello!</h1>
-   
-    </main>
+    <section className="flex items-center justify-center bg-background h-[90vh]">
+      <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
+        <div className="max-w-3xl mx-auto text-center">
+          <div>
+            <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+              <span className="text-sm font-medium text-primary">
+                Sort your notes easily
+              </span>
+            </span>
+            <h1 className="mt-8 text-3xl lg:text-6xl tracking-tight font-extrabold">
+              Create Notes With Ease
+            </h1>
+            <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              consequat lorem sed finibus pulvinar. Morbi pellentesque velit nec
+              enim mollis, a dictum velit semper. Fusce finibus tincidunt tortor
+              eget placerat.
+            </p>
+          </div>
+          <div className="flex justify-center max-w-sm mx-auto mt-10">
+            <RegisterLink>
+              <Button size="lg" className="w-full">
+                Sign Up For Free
+              </Button>
+            </RegisterLink>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
