5d3b1a5ca5cf7a767503e0e3b3a2c9680fcb5da1
Mark Slorach <hello@markslorach.com>
2024-03-01 20:02:05 +0000

    set up shadcn and theme switcher

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import { ThemeToggle } from "./components/ThemeToggle";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello!</h1>
+      <ThemeToggle/>
+    </main>
+  );
+}
